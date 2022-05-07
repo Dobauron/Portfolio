5d4a1dbd1a1650ac38f696869d197e69fb2ce76c
Dobauron <safetysafetystrazak@gmail.com>
2022-05-07 18:50:43 +0200

    Add all User register functionality.

--- my_portfolio/basic_functionality/urls.py
+++ my_portfolio/basic_functionality/urls.py
@@ -1,17 +1,25 @@
 from django.urls import path
-from . views import MainView
+from .views import MainView, dashboard, RegisterUser, cv_show, RegisterUserDone
 from django.contrib.auth import views as auth_views
 from django.urls import reverse_lazy
+
 urlpatterns = (
     path('', MainView.as_view(), name='main_view'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('password_change/', auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('basic_functionality:password_change_done')), name= 'password_change'),
+    path('password_change/',
+         auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('password_change_done')),
+         name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
-    path('dashboard/', views.dashboard, name = 'dashboard'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(success_url=reverse_lazy('basic_functionality:password_reset_done')), name='password_reset'),
+    path('dashboard/', dashboard, name='dashboard'),
+    path('password_reset/',
+         auth_views.PasswordResetView.as_view(success_url=reverse_lazy('password_reset_done')),
+         name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(success_url=reverse_lazy('basic_functionality:password_reset_complete')), name='password_reset_confirm'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        success_url=reverse_lazy('password_reset_complete')), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    path('register/', views.register, name='register')
+    path('register/', RegisterUser.as_view(), name='register'),
+    path('register/done/', RegisterUserDone.as_view(), name='register_done'),
+    path('Dobromir_Matuszak_CV/', cv_show, name='CV'),
 )
